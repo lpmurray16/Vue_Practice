2e9dbb855d13c0d172c945479bb9233645aa6a4c
Logan Murray <lpmurray16@users.noreply.github.com>
2021-06-22 23:40:30 -0500

    added a review component and form for reviews

--- First_Test/main.js
+++ First_Test/main.js
@@ -1,4 +1,74 @@
 // Practicing with Vue.js -- Logan Murray June 2021
+Vue.component('product-review', {
+    template: `
+    <form class="review-form" @submit.prevent="onSubmit">
+        
+        <p v-if="errors.length">
+            <b>Please correct the following error(s):</b>
+            <ul>
+                <li v-for="error in errors">{{ error }}</li>
+            </ul>
+        </p>
+
+        <h2>Leave a Review</h2>
+        <p>
+            <label for="name">Name:</label>
+            <input id="name" v-model="name">
+        </p>
+
+        <p>
+            <label for="review">Review:</label>
+            <textarea id="review" v-model="review"></textarea>
+        </p>
+
+        <p>
+            <label for="rating">Rating:</label>
+            <select id="rating" v-model.number="rating">
+                <option>5</option>
+                <option>4</option>
+                <option>3</option>
+                <option>2</option>
+                <option>1</option>
+            </select>
+        </p>
+
+        <p>
+            <input id="submit" type="submit" value="Submit">
+        </p>
+
+    </form>
+    `,
+    data() {
+        return {
+            name: null,
+            review: null,
+            rating: 5,
+            errors: []
+        }
+    },
+    methods: {
+        onSubmit() {
+            if (this.name && this.review && this.rating) {
+                let productReview = {
+                    name: this.name,
+                    review: this.review,
+                    rating: this.rating
+                }
+                this.$emit('review-submitted', productReview)
+                this.name = null
+                this.review = null
+                this.rating = null
+            } else {
+                if(!this.name) this.errors.push("Name required.")
+                if(!this.review) this.errors.push("Review required.")
+                if(!this.rating) this.errors.push("Rating required.")
+            }
+            
+        }
+    }
+})
+
+
 
 Vue.component('product', {
     props: {
@@ -43,6 +113,20 @@ Vue.component('product', {
                     :class="{ disabledButton: !InStock }">Add to Cart</button>
                     <button v-on:click="removeFromCart">Remove From Cart</button>
                 </div>
+                
+                <product-review @review-submitted="addReview"></product-review>
+
+                <div>
+                    <h2>Reviews</h2>
+                    <p v-if="!reviews.length">There are no reviews yet.</p>
+                    <ul>
+                        <li v-for="review in reviews">
+                            <p>{{ review.name }}</p>
+                            <p>Rating: {{ review.rating }}</p>
+                            <p>{{ review.review }}</p>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     `,
@@ -75,7 +159,8 @@ Vue.component('product', {
                     variantOnSale: true,
                     variantLink: "https://harrypottershop.co.uk/collections/wands/products/hermione-grangers-wand"
                 }
-            ]
+            ],
+            reviews: []
             
         }
     },
@@ -89,11 +174,14 @@ Vue.component('product', {
         updateProduct(index) {
             this.selectedVariant = index
             console.log(index)
-        } 
+        },
+        addReview(productReview) {
+            this.reviews.push(productReview)
+        }
     },
     computed: {
         title() {
-            return this.brand +' '+ this.variants[this.selectedVariant].variantName
+            return this.brand +' - '+ this.variants[this.selectedVariant].variantName
         },
         product_image() {
             return this.variants[this.selectedVariant].variantImage
